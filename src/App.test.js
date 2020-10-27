aae7d664c1166fd325decd20548e3443d63bbdf7
Rocio <ropiotti@gmail.com>
2020-10-27 15:06:00 +0100

    Setting up what tests we need

--- src/App.test.js
+++ src/App.test.js
@@ -4,3 +4,12 @@ import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
 
 Enzyme.configure({ adapter: new Adapter() });
 
+test("renders without error", () => {});
+
+test("renders button", () => {});
+
+test("renders counter display", () => {});
+
+test("counter starts at 0", () => {});
+
+test("clicking on button increments counter display", () => {});
