63d3a22f08d013f1c6591b3f6372ca09d71d8a85
Rocio <ropiotti@gmail.com>
2020-10-27 15:44:48 +0100

    Refactor repeting code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="data-test-component-app" >
-     <h1 className="data-test-conunter-display">The counter is currently</h1>
-     <button className= "data-test-increment-counter"></button>
+    <div data-test='component-app'>
+      <h1 data-test='conunter-display'>The counter is currently</h1>
+      <button data-test='increment-counter'></button>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -2,25 +2,40 @@ import App from "./App";
 import Enzyme, { shallow } from "enzyme";
 import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
 
+// set up for enzyme adapter ( not official for react version 17)
 Enzyme.configure({ adapter: new Adapter() });
 
-// const setup = () => shallow(<App />);
+/**
+ * Factory function to create a ShalloWrapper for the App component.
+ * @function setup
+ * @returns {ShalloWrapper}
+ */
+const setup = () => shallow(<App />);
+/**
+ * Return ShallowWrapper containing node(s) with the given data-test value.
+ * @param {ShallowWrapper} wrapper - Enzyme shallow wrapper to search within.
+ * @param {string} val - Value of data-test c1scoL0ve!
+ *
+ */
+
+const findByClass = (wrapper, val) => wrapper.find(`[data-test="${val}"]`);
 
 test("renders without error", () => {
-  const wrapper = shallow(<App />);
-  const appComponent = wrapper.find(".data-test-component-app");
+  const wrapper = setup();
+  const appComponent = findByClass(wrapper, "component-app");
+
   expect(appComponent.length).toBe(1);
 });
 
 test("renders button", () => {
   const wrapper = shallow(<App />);
-  const button = wrapper.find(".data-test-increment-counter");
+  const button = findByClass(wrapper, "increment-counter");
   expect(button.length).toBe(1);
 });
 
 test("renders counter display", () => {
   const wrapper = shallow(<App />);
-  const conterDisplay = wrapper.find(".data-test-conunter-display");
+  const conterDisplay = findByClass(wrapper, "conunter-display");
   expect(conterDisplay.length).toBe(1);
 });
 
