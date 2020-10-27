877dd14ab924c32c05d4b03e5636201f8e6fea18
Rocio <ropiotti@gmail.com>
2020-10-27 15:20:50 +0100

    First test

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App" data-test="">
+    <div className="data-test-component-app" >
      
     </div>
   );
--- src/App.test.js
+++ src/App.test.js
@@ -4,7 +4,11 @@ import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
 
 Enzyme.configure({ adapter: new Adapter() });
 
-test("renders without error", () => {});
+test("renders without error", () => {
+  const wrapper = shallow(<App/>);
+  const appComponent = wrapper.find(".data-test-component-app");
+  expect(appComponent.length).toBe(1);
+});
 
 test("renders button", () => {});
 
